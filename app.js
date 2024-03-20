10653812d32d2069a36b75c7c5b6aecdca72385e
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2024-03-20 12:18:02 +0200

    Revert "Hw05 avatars"

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-import 'dotenv/config'
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
-import usersRouter from './routes/usersRouter.js';
-import auth from './middleware/auth.js';
 
-import "./db.js";
 const app = express();
-
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts",auth, contactsRouter);
-app.use("/api/auth", usersRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
